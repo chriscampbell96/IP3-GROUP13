6760b48057a3a2faaa99866f32e5d9acb3185ac8
chriscampbell96 <chriscampbell.info@gmail.com>
2017-02-20 18:59:43 +0000

    Database Folder & SQL
    addition of database folder and sql

---
+++ sql/ip3_g13.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2017 at 07:58 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ip3_g13`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl`
+--
+
+CREATE TABLE `acl` (
+  `ai` int(10) UNSIGNED NOT NULL,
+  `action_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl_actions`
+--
+
+CREATE TABLE `acl_actions` (
+  `action_id` int(10) UNSIGNED NOT NULL,
+  `action_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
+  `action_desc` varchar(100) NOT NULL COMMENT 'Human readable description',
+  `category_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl_categories`
+--
+
+CREATE TABLE `acl_categories` (
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `category_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
+  `category_desc` varchar(100) NOT NULL COMMENT 'Human readable description'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_sessions`
+--
+
+CREATE TABLE `auth_sessions` (
+  `id` varchar(128) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ip_address` varchar(45) NOT NULL,
+  `user_agent` varchar(60) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `auth_sessions`
+--
+
+INSERT INTO `auth_sessions` (`id`, `user_id`, `login_time`, `modified_at`, `ip_address`, `user_agent`) VALUES
+('nv0u9fruil3kkccndjkna2t7ijjtltum', 1025018533, '2017-02-20 14:17:38', '2017-02-20 13:17:38', '::1', 'Spartan 14.14393 on Windows 10'),
+('eb99781rfhn02i4hpkthlnideuja0ljb', 1025018533, '2017-02-20 14:17:51', '2017-02-20 13:17:51', '::1', 'Spartan 14.14393 on Windows 10'),
+('7ucb9h75pmgp2frfdr586ferbsvnmubg', 1025018533, '2017-02-20 14:18:11', '2017-02-20 13:18:11', '::1', 'Spartan 14.14393 on Windows 10'),
+('tkemkvisesa2rfe1qetc7s4l1ftbr0da', 1025018533, '2017-02-20 14:18:42', '2017-02-20 13:18:42', '::1', 'Spartan 14.14393 on Windows 10'),
+('6vh8j7jhgu9q9sjd6i1158mfu4527cg6', 1025018533, '2017-02-20 14:19:19', '2017-02-20 13:19:19', '::1', 'Spartan 14.14393 on Windows 10'),
+('ar4gibc8lsc289c1p1agda8m0112in16', 1025018533, '2017-02-20 14:20:02', '2017-02-20 13:20:02', '::1', 'Spartan 14.14393 on Windows 10'),
+('f06239lss5hfrcr11prv655jknqiu4k0', 1025018533, '2017-02-20 14:20:43', '2017-02-20 13:20:43', '::1', 'Spartan 14.14393 on Windows 10'),
+('5qf3uic245nb7hlhpk3u7dr8calu0br7', 1025018533, '2017-02-20 15:34:55', '2017-02-20 14:41:35', '::1', 'Spartan 14.14393 on Windows 10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `denied_access`
+--
+
+CREATE TABLE `denied_access` (
+  `ai` int(10) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL,
+  `reason_code` tinyint(1) UNSIGNED DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ips_on_hold`
+--
+
+CREATE TABLE `ips_on_hold` (
+  `ai` int(10) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_errors`
+--
+
+CREATE TABLE `login_errors` (
+  `ai` int(10) UNSIGNED NOT NULL,
+  `username_or_email` varchar(255) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `username_or_email_on_hold`
+--
+
+CREATE TABLE `username_or_email_on_hold` (
+  `ai` int(10) UNSIGNED NOT NULL,
+  `username_or_email` varchar(255) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(12) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `auth_level` tinyint(3) UNSIGNED NOT NULL,
+  `banned` enum('0','1') NOT NULL DEFAULT '0',
+  `passwd` varchar(60) NOT NULL,
+  `passwd_recovery_code` varchar(60) DEFAULT NULL,
+  `passwd_recovery_date` datetime DEFAULT NULL,
+  `passwd_modified_at` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `auth_level`, `banned`, `passwd`, `passwd_recovery_code`, `passwd_recovery_date`, `passwd_modified_at`, `last_login`, `created_at`, `modified_at`) VALUES
+(1025018533, 'skunkbot', 'skunkbot@example.com', 1, '0', '$2y$11$FfFDL0wexVVYTZQ.mwErc.bJMZEaCSkhksVQ4bIFgVyuFuH51R.Sa', NULL, NULL, NULL, '2017-02-20 16:41:08', '2017-02-20 14:13:39', '2017-02-20 15:41:08');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `ca_passwd_trigger` BEFORE UPDATE ON `users` FOR EACH ROW BEGIN
+    IF ((NEW.passwd <=> OLD.passwd) = 0) THEN
+        SET NEW.passwd_modified_at = NOW();
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `acl`
+--
+ALTER TABLE `acl`
+  ADD PRIMARY KEY (`ai`),
+  ADD KEY `action_id` (`action_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `acl_actions`
+--
+ALTER TABLE `acl_actions`
+  ADD PRIMARY KEY (`action_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `acl_categories`
+--
+ALTER TABLE `acl_categories`
+  ADD PRIMARY KEY (`category_id`),
+  ADD UNIQUE KEY `category_code` (`category_code`),
+  ADD UNIQUE KEY `category_desc` (`category_desc`);
+
+--
+-- Indexes for table `auth_sessions`
+--
+ALTER TABLE `auth_sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
+
+--
+-- Indexes for table `denied_access`
+--
+ALTER TABLE `denied_access`
+  ADD PRIMARY KEY (`ai`);
+
+--
+-- Indexes for table `ips_on_hold`
+--
+ALTER TABLE `ips_on_hold`
+  ADD PRIMARY KEY (`ai`);
+
+--
+-- Indexes for table `login_errors`
+--
+ALTER TABLE `login_errors`
+  ADD PRIMARY KEY (`ai`);
+
+--
+-- Indexes for table `username_or_email_on_hold`
+--
+ALTER TABLE `username_or_email_on_hold`
+  ADD PRIMARY KEY (`ai`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `acl`
+--
+ALTER TABLE `acl`
+  MODIFY `ai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `acl_actions`
+--
+ALTER TABLE `acl_actions`
+  MODIFY `action_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `acl_categories`
+--
+ALTER TABLE `acl_categories`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `denied_access`
+--
+ALTER TABLE `denied_access`
+  MODIFY `ai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ips_on_hold`
+--
+ALTER TABLE `ips_on_hold`
+  MODIFY `ai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `login_errors`
+--
+ALTER TABLE `login_errors`
+  MODIFY `ai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `username_or_email_on_hold`
+--
+ALTER TABLE `username_or_email_on_hold`
+  MODIFY `ai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `acl`
+--
+ALTER TABLE `acl`
+  ADD CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`action_id`) REFERENCES `acl_actions` (`action_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `acl_actions`
+--
+ALTER TABLE `acl_actions`
+  ADD CONSTRAINT `acl_actions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `acl_categories` (`category_id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
