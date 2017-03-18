068db5a484b87fef0542bf3a3b74011aa69d7efc
Heather <heathz2222@gmail.com>
2017-03-18 17:39:13 +0000

    footer
    :)

--- dashboardAdmin.php
+++ dashboardAdmin.php
@@ -178,15 +178,14 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
   </div>
 </div>
 </div>
+<?php include 'templates/footer.php';?>
+
+
     <!-- jQuery -->
     <script src="templates/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="templates/js/bootstrap.min.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="templates/js/plugins/morris/raphael.min.js"></script>
-    <script src="templates/js/plugins/morris/morris.min.js"></script>
-    <script src="templates/js/plugins/morris/morris-data.js"></script>
   </body>
 </html>
