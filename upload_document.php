b85e6d1102a32cc037241548740ed6ad78d6d065
Heather <heathz2222@gmail.com>
2017-03-19 20:01:03 +0000

    html body tag closed
    :)

--- upload_document.php
+++ upload_document.php
@@ -119,9 +119,11 @@ if(isset($_POST['btn-upload']))
 </div>
 <?php include 'templates/foot.php';?>
 
+<!-- jQuery -->
+<script src="templates/js/jquery.js"></script>
 
-    <!-- jQuery -->
-    <script src="templates/js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="templates/js/bootstrap.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="templates/js/bootstrap.min.js"></script>
+  </body>
+</html>
