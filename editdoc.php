306784640000e38889ca6072cd4479d3b85c41c0
Heather <heathz2222@gmail.com>
2017-04-11 13:07:46 +0100

    center buttons
    tfygvhjnm

--- editdoc.php
+++ editdoc.php
@@ -256,7 +256,7 @@ if(isset($msg))
          <td><?php echo $row['revDesc']?></td>
          <td><?php echo $row['revFile']?></td>
          <td><?php echo $row['revStatus']?></td>
-         <td>
+         <td style="padding:10px; text-align:center; align-items:center;">
            <a href="activaterevision.php?revision_id=<?php print($rid); ?>" class="btn" style="background-color:#f05133; color:white; margin-top:10px; margin-bottom:10px; border-radius:10px;"><i class="fa fa-fw fa-toggle-right"></i> Activate Revision</a>
 
             <?php
